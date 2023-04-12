666d89a3ac48e1a24533b7fe6abdb7217da7569d
JamesPartsafas <56362880+JamesPartsafas@users.noreply.github.com>
2023-04-12 10:52:44 -0400

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,23 @@
+import wixData from 'wix-data';
+import wixMembers from 'wix-members';
+
+$w.onReady(function () {
+	let query = wixData.query("UserSettings");
+	wixMembers.currentMember.getMember().then( (member) => {
+		let email = member.loginEmail;
+		query.eq("userId", email).find().then((results) => {
+			console.log(email);
+			console.log(results);
+			if(results.items.length > 0) {
+				let settings = results.items[0];
+				let phoneOn = settings["phone"];
+				let emailOn = settings["email"];
+				let baby = settings["baby"];
+                if (baby == null || baby == "")
+                    $w('#text16').text = "Baby Izzy";
+                else
+				    $w('#text16').text = baby;
+			}
+		})
+	} );
+});
