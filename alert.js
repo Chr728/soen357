4e538a1634330af1cc04eac7d59dcb7eb5cffafa
JamesPartsafas <56362880+JamesPartsafas@users.noreply.github.com>
2023-04-15 18:00:38 -0400

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,3 +1,4 @@
+// Used in alert page, which is accessed from the camera page to run an audio cue, alerting caretakers of a danger to their child.
 $w.onReady(function () {
     $w('#audioPlayer1').play();
 });
